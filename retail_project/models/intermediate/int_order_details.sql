8fc75fc12c498412e69ece1e5a2002dd78723bfd
narasingha-g <narasinghagh2052@gmail.com>
2026-07-13 14:43:32 +0530

    Finalize intermediate order details model with data quality checks

--- retail_project/models/intermediate/int_order_details.sql
+++ retail_project/models/intermediate/int_order_details.sql
@@ -1,57 +1,98 @@
+WITH
+
+orders AS (
+
+    SELECT *
+    FROM {{ ref('stg_orders') }}
+
+),
+
+order_items AS (
+
+    SELECT *
+    FROM {{ ref('stg_order_items') }}
+
+),
+
+payment_summary AS (
+
+    SELECT
+        ORDER_ID,
+        SUM(PAYMENT_VALUE) AS PAYMENT_VALUE,
+        MAX(PAYMENT_TYPE) AS PAYMENT_TYPE,
+        MAX(PAYMENT_INSTALLMENTS) AS PAYMENT_INSTALLMENTS
+    FROM {{ ref('stg_order_payments') }}
+    GROUP BY ORDER_ID
+
+),
+
+customers AS (
+
+    SELECT *
+    FROM {{ ref('stg_customers') }}
+
+),
+
+products AS (
+
+    SELECT *
+    FROM {{ ref('stg_products') }}
+
+),
+
+sellers AS (
+
+    SELECT *
+    FROM {{ ref('stg_sellers') }}
+
+)
+
 SELECT
+-- Order Information 
+o.ORDER_ID,
+o.CUSTOMER_ID,
+o.ORDER_STATUS,
+o.ORDER_PURCHASE_TIMESTAMP,
+o.ORDER_APPROVED_AT,
+o.ORDER_DELIVERED_CARRIER_DATE,
+o.ORDER_DELIVERED_CUSTOMER_DATE,
+o.ORDER_ESTIMATED_DELIVERY_DATE,
+-- Item Information
+oi.ORDER_ITEM_ID,
+oi.PRODUCT_ID,
+oi.SELLER_ID,
+oi.PRICE,
+oi.FREIGHT_VALUE,
+-- Payment Information
+ps.PAYMENT_TYPE,
+ps.PAYMENT_INSTALLMENTS,
+ps.PAYMENT_VALUE,
+-- Customer Information
+c.CUSTOMER_CITY,
+c.CUSTOMER_STATE,
+-- Seller Information
+s.SELLER_CITY,
+s.SELLER_STATE,
+-- Product Information
+p.PRODUCT_CATEGORY_NAME,
+p.PRODUCT_WEIGHT_G,
+p.PRODUCT_LENGTH_CM,
+p.PRODUCT_WIDTH_CM,
+p.PRODUCT_HEIGHT_CM 
 
-    -- Order Information
-    o.ORDER_ID,
-    o.CUSTOMER_ID,
-    o.ORDER_STATUS,
-    o.ORDER_PURCHASE_TIMESTAMP,
-    o.ORDER_APPROVED_AT,
-    o.ORDER_DELIVERED_CARRIER_DATE,
-    o.ORDER_DELIVERED_CUSTOMER_DATE,
-    o.ORDER_ESTIMATED_DELIVERY_DATE,
+FROM orders o
 
-    -- Item Information
-    oi.ORDER_ITEM_ID,
-    oi.PRODUCT_ID,
-    oi.SELLER_ID,
-    oi.PRICE,
-    oi.FREIGHT_VALUE,
-
-    (oi.PRICE + oi.FREIGHT_VALUE) AS TOTAL_ITEM_AMOUNT,
-
-    -- Payment Information
-    op.PAYMENT_TYPE,
-    op.PAYMENT_INSTALLMENTS,
-    op.PAYMENT_VALUE,
-
-    -- Customer Information
-    c.CUSTOMER_CITY,
-    c.CUSTOMER_STATE,
-
-    -- Seller Information
-    s.SELLER_CITY,
-    s.SELLER_STATE,
-
-    -- Product Information
-    p.PRODUCT_CATEGORY_NAME,
-    p.PRODUCT_WEIGHT_G,
-    p.PRODUCT_LENGTH_CM,
-    p.PRODUCT_WIDTH_CM,
-    p.PRODUCT_HEIGHT_CM
-
-FROM {{ ref('stg_orders') }} o
-
-LEFT JOIN {{ ref('stg_order_items') }} oi
+LEFT JOIN order_items oi
        ON o.ORDER_ID = oi.ORDER_ID
 
-LEFT JOIN {{ ref('stg_order_payments') }} op
-       ON o.ORDER_ID = op.ORDER_ID
+LEFT JOIN payment_summary ps
+       ON o.ORDER_ID = ps.ORDER_ID
 
-LEFT JOIN {{ ref('stg_customers') }} c
+LEFT JOIN customers c
        ON o.CUSTOMER_ID = c.CUSTOMER_ID
 
-LEFT JOIN {{ ref('stg_products') }} p
+LEFT JOIN products p
        ON oi.PRODUCT_ID = p.PRODUCT_ID
 
-LEFT JOIN {{ ref('stg_sellers') }} s
+LEFT JOIN sellers s
        ON oi.SELLER_ID = s.SELLER_ID
